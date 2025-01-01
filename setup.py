2e0ed068e5bca94257082c94445a0990e58d5060
HiroseSadao <hirose.akka@gmail.com>
2025-01-01 17:50:28 +0900

    Fix setup.py to remove top-level import numpy

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-import numpy as np
 import os.path as osp
 from setuptools import setup, find_packages
 from distutils.extension import Extension
@@ -6,40 +5,43 @@ from Cython.Build import cythonize
 
 def readme():
     with open('README.md') as f:
-        content = f.read()
-    return content
-
-
-def numpy_include():
-    try:
-        numpy_include = np.get_include()
-    except AttributeError:
-        numpy_include = np.get_numpy_include()
-    return numpy_include
-
-
-ext_modules = [
-    Extension(
-        'prtreid.metrics.rank_cylib.rank_cy',
-        ['prtreid/metrics/rank_cylib/rank_cy.pyx'],
-        include_dirs=[numpy_include()],
-    )
-]
-
+        return f.read()
 
 def get_requirements(filename='requirements.txt'):
     here = osp.dirname(osp.realpath(__file__))
     with open(osp.join(here, filename), 'r') as f:
-        requires = [line.replace('\n', '') for line in f.readlines()]
-    return requires
+        return [line.strip() for line in f.readlines()]
+
+def build_ext_modules():
+    # ここで初めて numpy を import する
+    import numpy as np
+
+    def numpy_include():
+        try:
+            return np.get_include()
+        except AttributeError:
+            return np.get_numpy_include()
+
+    ext_modules = [
+        Extension(
+            'prtreid.metrics.rank_cylib.rank_cy',
+            ['prtreid/metrics/rank_cylib/rank_cy.pyx'],
+            include_dirs=[numpy_include()],
+        )
+    ]
+    # 必要ならオプションを追加
+    return cythonize(ext_modules)
 
 setup(
-    description='A library for deep learning person re-ID in PyTorch',
-    license='MIT',
+    name="prtreid",
+    version="0.0.1",
+    description="A library for deep learning person re-ID in PyTorch",
+    license="MIT",
     long_description=readme(),
     packages=find_packages(),
     install_requires=get_requirements(),
     extras_require={"labels": get_requirements("requirements_labels.txt")},
-    keywords=['Person Re-Identification', 'Deep Learning', 'Computer Vision'],
-    ext_modules=cythonize(ext_modules)
+    keywords=["Person Re-Identification", "Deep Learning", "Computer Vision"],
+    # ext_modules をビルド時に初めて呼ぶ
+    ext_modules=build_ext_modules()
 )
